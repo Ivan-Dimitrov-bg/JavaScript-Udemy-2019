be4fd46259d1af6912ba1f805b54a212d9d66028
Ivan Dimitrov <Ivan.Dimitrov.bg@gmail.com>
2019-02-23 13:33:26 +0200

    DOM-pig-game finished

--- Section 2/4-DOM-pig-game/starter/challenge123.js
+++ Section 2/4-DOM-pig-game/starter/challenge123.js
@@ -1,42 +1,10 @@
-/*
-GAME RULES:
+var scores, roundScore, activePlayer, gamePlaying, lastDice;
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-//01.
-/*
-
-    // How to create our fundamental game variables;
-    // How to generate a random number;
-    // How to manipulate the DOM;
-    // How to read from the DOM;
-    // How to change CSS Style.
-
-*/
-var scores, roundScore, activePlayer, gamePlaying;
-
-/*
-
-    ** Events ** : Notifications that are sent to notify the code that something happened on the webpage;
-        Clicking a button, resizing a window, scrolling down or pressing a key;
-    ** Event listener ** : a function that performs an action based on a certain event. It waits for a specific event to happen.
-    ***https://developer.mozilla.org/en-US/docs/Web/Events
-    
-    ** An anonymous function ** is a function that was declared without any named identifier to refer to it. - 
-*/
-
-/*
-function btn(){
+function btn() {
     //do something here
     
 }
 btn();
-*/
 
 init();
 
@@ -44,18 +12,20 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 
     if (gamePlaying) {
     //1. Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
-
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+        
     //2. Display the result     
-        var diceDom =  document.querySelector('.dice');
-        diceDom.style.display = 'block';
-        diceDom.src = 'dice-' + dice + '.png';
-
-    //3. Update the round score If the rolled number was
-        if (dice !== 1) {
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-1').src = 'dice-' + dice2 + '.png';
+        
+        
+        if (dice1 !== 1 && dice2 != 1) {
 
             // Add score
-            roundScore += dice;
+            roundScore += dice1 + dice2;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
              //alert(roundScore);
         } else {
@@ -63,19 +33,30 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 
             nextPlayer();
         }
-        
+    }});
+    //3. Update the round score If the rolled number was
+       /*
+        if (dice === 6 && lastDice === 6) {
+            //Player looses score
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0';
+            lastDice = 0;
+            nextPlayer();
+        }else if (dice !== 1) {
+
+            // Add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+             //alert(roundScore);
+        } else{
+            // Next player
+
+            nextPlayer();
+        }   
     }
+    lastDice = dice;
+    */
 
-        
-});
-
-
-/*
-    lesson 58
-    ** What the ternary operator is;
-    
-    ** How to add, remove and toggle HTML classes.
-*/
 document.querySelector('.btn-hold').addEventListener('click', function () {
     
      if (gamePlaying) {
@@ -85,12 +66,21 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         // update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
    
-        // check if player won the game
-
-        if (scores[activePlayer] >= 100) {
+        var input = document.querySelector('.final-score').value;
+         var winningScore;
+     
+        if(input){
+            var winningScore = input;
+        }else{
+            winningScore = 100;    
+        }
+         
+        if (scores[activePlayer] >= winningScore) {
 
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
+            
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
@@ -98,8 +88,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
             nextPlayer();
         }
      }
-    
-    
+
 });
 
 function nextPlayer() {
@@ -114,7 +103,8 @@ function nextPlayer() {
     //document.querySelector('.player-0-panel').classList.remove('active');
     //document.querySelector('.player-1-panel').classList.add('active');
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
     
 }
 
@@ -126,7 +116,8 @@ function init() {
     activePlayer = 0;
     roundScore = 0;
     gamePlaying = true;
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
     
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -142,30 +133,3 @@ function init() {
     document.querySelector('.player-0-panel').classList.add('active');
     document.querySelector('.player-1-panel').classList.remove('active');
 }
-
-
-
-
-
-
-
-
-
-
-
-//dice = Math.floor(Math.random() * 6) + 1;
-//'only text'
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//'we can use html tags'
-//document.querySelector('#current-' + activePlayer).InnerHTML = '<em>' + dice + </em>
-
-//var x = document.querySelector('#score-0').textContent;
-
-//'console.log(x);
-
-
-
-
-
-
-
